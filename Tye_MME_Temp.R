0b2bec66a20c375a24d2272e2daffad14bc3a56c
Simon Tye <simontye@uark.edu>
2020-03-17 20:20:34 -0500

    Added some MN events; began adding Till workflow

--- Tye_MME_Temp.R
+++ Tye_MME_Temp.R
@@ -31,8 +31,10 @@
 # Reset global enviroment
 rm(list=ls())
 
+# Install USGS Lake Attributes
 #remotes::install_github("USGS-R/lakeattributes")
 
+# Load packages
 library(plyr)
 library(dplyr)
 library(ggplot2)
@@ -49,17 +51,25 @@ library(usmap)
 library(ggmap)
 library(ggrepel)
 library(ggspatial)
+library(rgdal)
+library(stringr)
+library(lubridate)
+library(raster)
+library(naniar)
 
 ###############################################################
 
+# Set working directory
 setwd("/Users/simontye/Documents/Research/Projects/MME_Temp/2020_MME_Temp")
 
+# Load files
 MN.Fish  <- read.csv(file = "data/raw/MN_Fish_Final.csv", head = TRUE, sep = ",")
 MN.Site  <- read.csv(file = "data/raw/MN_Site_County.csv", head = TRUE, sep = ",")
 WI.Fish  <- read.csv(file = "data/raw/WI_Fish_Final.csv", head = TRUE, sep = ",")
 WI.Site  <- read.csv(file = "data/raw/WI_Site.csv", head = TRUE, sep = ",")
 Site.ID  <- read.csv(file = "data/raw/Site_ID.csv", head = TRUE, sep = ",")
-#thermal  <- read.csv(file = "data/raw/thermal_metrics.csv")
+thermal  <- read.csv(file = "data/raw/thermal_metrics.csv")
+thermal  <- inner_join(thermal, Site.ID, by = "site_id")
 
 ###############################################################
 ### Preparing MN database
@@ -72,7 +82,7 @@ MN.Fish.Columns <- c(1, 70:71)
 MN.Fish.GPS <- MN.Fish[, MN.Fish.Columns]
 MN.Fish.GPS$Lat <- as.numeric(as.character(MN.Fish.GPS$Lat))
 
-# Remove events without coordinates (284 events total; 229 events with coordinates)
+# Remove events without coordinates (284 events total; 240 events with coordinates)
 MN.Fish.GPS <- na.omit(MN.Fish.GPS)
 
 # Save event order for later
@@ -110,7 +120,7 @@ colnames(MN.Fish.Final)[c(82:83)] <- c("Lat_OG", "Long_OG")
 # Remove unnecessary columns
 MN.Fish.Final[,c("Site", "Lat.x", "Long.x", "Prmnn_I",
                  "GNIS_ID", "GNIS_Nm", "ReachCd",
-                 "FType", "FCode", "WBIC")] <- NULL
+                 "FType", "FCode", "WBIC", "GPS", "Notes")] <- NULL
 
 # Change column format
 MN.Fish.Final$Lat_OG <- as.numeric(as.character(MN.Fish.Final$Lat_OG))
@@ -131,12 +141,13 @@ WI.Fish.1 <- merge(WI.Site, Site.ID, by = "site_id", all.x = FALSE, all.y = TRUE
 # Merge WI site data and Till et al. (2019) dataset
 WI.Fish.2 <- merge(WI.Fish.1, WI.Fish, by = "WBIC", all.x = FALSE, all.y = TRUE)
 
-# Remove sites without data (just to double-check)
+# Remove sites without data (none removed)
 WI.Fish.Final <- subset(WI.Fish.2, !is.na(WI.Fish.2$Event))
 
 # Remove unnecessary columns
 WI.Fish.Final[,c("WBIC", "X", "Site", "Prmnn_I", "GNIS_ID",
-                 "GNIS_Nm", "ReachCd", "FType", "FCode")] <- NULL
+                 "GNIS_Nm", "ReachCd", "FType", "FCode",
+                 "Snail", "Crayfish", "Frogs")] <- NULL
 
 # Remove unnecessary dataframes
 rm(WI.Fish, WI.Fish.1, WI.Fish.2, WI.Site, Site.ID)
@@ -193,16 +204,13 @@ Fish.Final$Salmonidae     <- ifelse(Fish.Final$Brown.Trout == 1, 1,
                                       ifelse(Fish.Final$Trout == 1, 1,
                                              ifelse(Fish.Final$Whitefish == 1, 1, 0))))
 
-# Should we combine "Drum" and "Freshwater.Drum"?
 Fish.Final$Sciaenidae     <- ifelse(Fish.Final$Drum == 1, 1,
                                ifelse(Fish.Final$Freshwater.Drum == 1, 1, 0))
 
-# Order events by date and add sequence of events to remove duplicate events
+## Order events by date and add sequence of events to remove duplicate events
 Fish.Final$Investigation.Start.Date <- as.Date(Fish.Final$Investigation.Start.Date, format = "%d-%b-%y")
 Fish.Final <- Fish.Final[order(as.Date(Fish.Final$Investigation.Start.Date, format="%d-%b-%y")),]
 Fish.Final$Fishkill.Inv.Seq.No <- c(1:915)
-
-
 
 # Remove unnecessary columns
 Fish.Final[,c("Site.Seq.No", "Swims.Station.Id", "Stream.Miles.or.Lake.Acres.Affected",
@@ -213,7 +221,10 @@ Fish.Final$Station.Name <- toupper(Fish.Final$Station.Name)
 Fish.Final$Cause.Detail <- toupper(Fish.Final$Cause.Detail)
 
 # Export Fish.Final dataset
-write.csv(Fish.Final, "data/raw/Fish_Final_20200315.csv", row.names = TRUE)
+write.csv(Fish.Final, "data/raw/Fish_Final.csv", row.names = TRUE)
+
+# Remove unnecessary dataframes
+rm(MN.Fish.Final, WI.Fish.Final)
 
 ###############################################################
 ### Preliminary figures
@@ -227,7 +238,7 @@ Fish.Map$Year <- ifelse(Fish.Map$Year > 2000, Fish.Map$Year, NA)
 Fish.Map <- na.omit(Fish.Map)
 
 # Create new event column
-Fish.Map$Event <- c(1:635)
+Fish.Map$Event <- c(1:646)
 
 #Fish.Map$Year <- as.numeric(Fish.Map$Year)
 # Rename columns to match ggplot functions
@@ -258,5 +269,196 @@ ggplot(data = states, mapping = aes(x = long, y = lat, group = group)) +
   stat_density2d(data = Fish.Map, aes(x = long, y = lat, fill = ..level.., alpha = ..level.., group = region), bins = 30, geom = "polygon") +
   geom_point(data = Fish.Map, color = "black", fill = "darkolivegreen3", size = 2, shape = 21, aes(x = long, y = lat, group = region)) +
   theme_nothing()
+
+# Remove map dataframes
+rm(counties, states)
+
+###############################################################
+###############################################################
+### Till et al. (2019) framework
+###############################################################
+###############################################################
+
+###############################################################
+### MME raw data
+###############################################################
+
+MME <- Fish.Final %>%
+  rename_all(tolower) %>%
+  mutate_all(tolower) %>%
+  #dplyr::filter(min.kill.size!="excludable") %>% # Need to determine cutoff
+  dplyr::select(site_id, # Changed from WBIC
+                year,
+                investigation.start.month,
+                fishkill.inv.seq.no,
+                cause.group) %>% # change from cause.category to cause.group
+  rename(month = investigation.start.month) %>%
+  mutate(dummy = 1,
+         cause.categories = cause.group) %>% # change from cause.category to cause.group
+  spread(cause.categories, dummy, fill = 0) %>%
+  dplyr::select(-fishkill.inv.seq.no) %>%
+  distinct(site_id, year, month, .keep_all = TRUE)
+
+###############################################################
+### Thermal raw data
+###############################################################
+
+thermal <- thermal %>%
+  rename_all(tolower) #%>%
+  mutate(year = as.numeric(year)) #%>%
+  filter(year >= 2003) #%>%
+  #mutate(year = as.character(year)) #%>%
+  #dplyr::select(-contains("strat"),
+                #-sthermo_depth_mean)
+
+###############################################################
+
+thermal_annual <- thermal %>%
+  dplyr::select(-contains('jas'),
+                -starts_with('mean_surf_'),
+                -starts_with('mean_bot_'), 
+                -starts_with('max_surf_'), 
+                -starts_with('max_bot_')) %>%
+  group_by(site_id, year) %>%
+  summarise_if(is.numeric, mean, na.rm = TRUE) %>%
+  ungroup() %>%
+  rename(ice_duration = ice_duration_days,
+         schmidt = schmidt_daily_annual_sum,
+         variance_after_ice_30 = coef_var_0.30, 
+         variance_after_ice_60 = coef_var_30.60, 
+         cumulative_above_0 = gdd_wtr_0c,
+         cumulative_above_5 = gdd_wtr_5c,
+         cumulative_above_10 = gdd_wtr_10c) %>%
+  mutate(log_schmidt = log(schmidt + .00001))
+
+###############################################################
+
+thermal_monthly <- thermal %>%
+  dplyr::select(starts_with('mean_surf_'),            # tidy the temp data
+                starts_with('mean_bot_'), 
+                starts_with('max_surf_'), 
+                starts_with('max_bot_'),
+                -contains('jas'),
+                year, site_id) %>%
+  mutate(uniqueid = 1:n()) %>%
+  gather(key = "type", value = "temperature", 
+         starts_with('mean_surf_'),
+         starts_with('mean_bot_'), 
+         starts_with('max_surf_'), 
+         starts_with('max_bot_')) %>%
+  separate(type, into=c('metric', 'depth', 'month'), sep='_')  %>%
+  unite(metric, metric, depth) %>%
+  spread(metric, temperature) %>%
+  dplyr::select(-uniqueid) %>%
+  group_by(site_id, year, month) %>%                      # average over sites
+  summarise_if(is.numeric, mean, na.rm = TRUE) %>%
+  ungroup() %>%
+  arrange(site_id, year, month) %>%
+  mutate(date = ymd(paste(year, month, "15"))) %>%
+  filter(date > "2003-01-01", date < "2014-05-01")
+
+###############################################################
+
+thermal_monthly <- thermal_monthly %>%
+  group_by(site_id, month) %>%
+  mutate(max_bot_z = scale(max_bot),                   # make z-score vars
+         max_surf_z = scale(max_surf),
+         mean_bot_z = scale(mean_bot),
+         mean_surf_z = scale(mean_surf)) %>%
+  ungroup() %>%
+  mutate(layer_diff = mean_surf - mean_bot,            # create additional temp/time features
+         quadratic_temp = mean_surf^2,
+         season = fct_collapse(month,
+                               "winter" = "dec",
+                               "winter" = "jan",
+                               "winter" = "feb",
+                               "spring" = "mar",
+                               "spring" = "apr",
+                               "spring" = "may",
+                               "summer" = "jun",
+                               "summer" = "jul",
+                               "summer" = "aug",
+                               "fall"   = "sep",
+                               "fall"   = "oct",
+                               "fall"   = "nov"))
+
+###############################################################
+
+pr <- thermal_monthly %>%
+  dplyr::select(max_surf, mean_surf, mean_bot) %>%
+  prcomp(center = TRUE, scale = TRUE)
+
+temp <- thermal_monthly %>%
+  dplyr::select(max_surf, mean_surf, mean_bot) %>%
+  as.matrix() %*% pr$rotation[,1]
+
+thermal_monthly <- thermal_monthly %>%
+  add_column(temp)
+
+rm(pr, temp, thermal)
+
+###############################################################
+### Merging MME into thermal
+###############################################################
+
+# Change to numeric or characters, whichever works
+thermal_annual$year  <- as.numeric(thermal_annual$year)
+thermal_monthly$year <- as.numeric(thermal_monthly$year)
+MME$year             <- as.numeric(MME$year)
+### Site id also needs to be changes
+
+df <- thermal_monthly %>%
+  left_join(thermal_annual, by = c("year", "site_id")) %>%
+  left_join(MME, by = c("year", "month", "site_id"))
+
+df <- df %>%
+  mutate(summerkill = ifelse(is.na(summerkill), 0, summerkill),
+         winterkill = ifelse(is.na(winterkill), 0, winterkill),
+         anthropogenic = ifelse(is.na(anthropogenic), 0, anthropogenic),
+         infectious = ifelse(is.na(infectious), 0, infectious),
+         unknown = ifelse(is.na(unknown), 0, unknown))
+
+rm(thermal_monthly, thermal_annual)
+
+###############################################################
+### Snowfall data
+###############################################################
+
+tidy_snow <- function(path) {
+  
+  e <-extent(-92.9, -87, 42.4 , 46.9)
+  a <- crop(raster(path),e)
+  
+  a1 <- as.data.frame(coordinates(a))
+  a2<- as.data.frame(a)
+  
+  data <- na.omit(cbind(a1, a2)) 
+  
+  names(data) <- c('x', 'y', 'snow')
+  
+  data$long_round <- round(data$x, 1)
+  data$lat_round <- round(data$y, 1)  
+  
+  data_output <- data %>%
+    group_by(long_round, lat_round) %>%
+    summarise(Snow = mean(snow))
+  
+  data_output$Year <- str_sub(path, 24, 27)
+  data_output$Month <- str_sub(path, 28, 29)
+  return(data_output)
+}
+
+###############################################################
+###############################################################
+###############################################################
+
+setwd("data/raw/PRISM_precip")
+file_names <- list.files() %>%
+  str_subset("asc$")
+snow_data <- map_df(file_names, tidy_snow)
+
+write_csv(snow_data, "../../processed/snow_data.csv")
+
+rm(snow_data)
 
 
